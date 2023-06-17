58922b021671614a6095eea96bc92ee69cd6c07c
Rishikesh Lingayat <rishikeshlingayat09@gmail.com>
2023-06-18 01:57:25 +0530

    FORMAT CHANGED

--- src/main/java/com/spitSpring/HiberanateHQL/controller/HibernateController.java
+++ src/main/java/com/spitSpring/HiberanateHQL/controller/HibernateController.java
@@ -3,9 +3,6 @@ package com.spitSpring.HiberanateHQL.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
